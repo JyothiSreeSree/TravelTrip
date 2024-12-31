9d9191cf65f1bbe31936c1d9788a031d18eb4d6f
jyothisree <jyothisreesldv1@gmail.com>
2024-12-31 01:54:30 +0000

    converted class component to functional component for login page

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,31 +1,30 @@
-import {Component} from 'react'
-import {Redirect} from 'react-router-dom'
+import {useState} from 'react'
+import {Redirect, useHistory} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import {IoEyeOutline, IoEyeOffOutline} from 'react-icons/io5'
 import './index.css'
 
-class LoginPage extends Component {
-  state = {
-    username: '',
-    password: '',
-    showErrMsg: false,
-    errorMsg: '',
-    showPassword: false,
-  }
+const LoginPage = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showErrMsg, setShowErrMsg] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
-  onSubmitSuccess = jwtToken => {
-    const {history} = this.props
+  const history = useHistory()
+
+  const onSubmitSuccess = jwtToken => {
     Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
     history.replace('/')
   }
 
-  onSubmitFailure = error => {
-    this.setState({showErrMsg: true, errorMsg: error})
+  const onSubmitFailure = error => {
+    setShowErrMsg(true)
+    setErrorMsg(error)
   }
 
-  onSubmitLogin = async event => {
+  const onSubmitLogin = async event => {
     event.preventDefault()
-    const {username, password} = this.state
     const userDetails = {username, password}
     const url = 'https://apis.ccbp.in/login'
     const options = {
@@ -35,84 +34,72 @@ class LoginPage extends Component {
     const response = await fetch(url, options)
     const data = await response.json()
     if (response.ok) {
-      this.onSubmitSuccess(data.jwt_token)
+      onSubmitSuccess(data.jwt_token)
     } else {
-      this.onSubmitFailure(data.error_msg)
+      onSubmitFailure(data.error_msg)
     }
   }
 
-  onChangeUsername = event => {
-    event.preventDefault()
-    this.setState({username: event.target.value})
+  const onShowPassword = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword)
   }
 
-  onChangePassword = event => {
-    event.preventDefault()
-    this.setState({password: event.target.value})
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken !== undefined) {
+    return <Redirect to="/" />
   }
 
-  onShowPassword = () => {
-    this.setState(prevState => ({showPassword: !prevState.showPassword}))
-  }
+  const buttonIcon = showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />
+  const passwordInputType = showPassword ? 'text' : 'password'
 
-  render() {
-    const {username, password, showErrMsg, errorMsg, showPassword} = this.state
-    const jwtToken = Cookies.get('jwt_token')
-    if (jwtToken !== undefined) {
-      return <Redirect to="/" />
-    }
+  return (
+    <div className="loginPageContainer">
+      <form className="loginContainer" onSubmit={onSubmitLogin}>
+        <h1 className="loginHeading">Travel Trip</h1>
+        <div className="usernameContainer">
+          <label htmlFor="username" className="usernameLabel">
+            Username
+          </label>
+          <input
+            className="usernameInput"
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="passwordContainer">
+          <label htmlFor="password" className="passwordLabel">
+            Password
+          </label>
 
-    const buttonIcon = showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />
-    const passwordInputType = showPassword ? 'text' : 'password'
-    return (
-      <div className="loginPageContainer">
-        <form className="loginContainer" onSubmit={this.onSubmitLogin}>
-          <h1 className="loginHeading">Travel Trip</h1>
-          <div className="usernameContainer">
-            <label htmlFor="username" className="usernameLabel">
-              Username
-            </label>
+          <div className="passwordInnerContainer">
             <input
-              className="usernameInput"
-              id="username"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={this.onChangeUsername}
+              className="passwordInput"
+              id="password"
+              type={passwordInputType}
+              placeholder="Password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
             />
+            <button
+              type="button"
+              data-testid="show-password"
+              onClick={onShowPassword}
+              className="passwordEye"
+            >
+              {buttonIcon}
+            </button>
           </div>
-          <div className="passwordContainer">
-            <label htmlFor="password" className="passwordLabel">
-              Password
-            </label>
-
-            <div className="passwordInnerContainer">
-              <input
-                className="passwordInput"
-                id="password"
-                type={passwordInputType}
-                placeholder="Password"
-                value={password}
-                onChange={this.onChangePassword}
-              />
-              <button
-                type="button"
-                data-testid="show-password"
-                onClick={this.onShowPassword}
-                className="passwordEye"
-              >
-                {buttonIcon}
-              </button>
-            </div>
-          </div>
-          {showErrMsg && <p className="errorMsg">{errorMsg}</p>}
-          <button type="submit" className="loginButton">
-            Login
-          </button>
-        </form>
-      </div>
-    )
-  }
+        </div>
+        {showErrMsg && <p className="errorMsg">{errorMsg}</p>}
+        <button type="submit" className="loginButton">
+          Login
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
